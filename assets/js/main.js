eabc2b368bfb825a3aba33a415fad008cf0d63d4
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-03-11 20:11:56 +0000

    Resized Google Maps and styled Google maps within Main.js, attempting to center Google maps, changed links into buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 52.20611, lng: 1.57757},
     zoom: 9,
+    mapId: '50f5268b5225a2a4',
   });
 }
 
