969caa7d81bb513cdd85b86567b5689e5fefebdb
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-03-12 14:30:06 +0000

    Attempting to create the JavaScript for the search on the map for certian locations within the suffolk coast area.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,32 @@
 let map;
+var service;
+var infowindow;
 
 function initMap() {
+    const suffolkCoastal = new google.maps.LatLng (52.1539301, 1.4746412);
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 52.20611, lng: 1.57757},
-    zoom: 9,
+    center: suffolkCoastal,
+    zoom: 10,
     mapId: '50f5268b5225a2a4',
   });
+var request = {
+    location: suffolkCoastal,
+    radius: 1000,
+    type: ['beach']
+};
+var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+}
+}
+
+
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
 
