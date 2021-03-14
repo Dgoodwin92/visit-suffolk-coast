c58566c5ca32ad6427311fc5da9269470692f708
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-03-14 22:32:33 +0000

    Figured out the correct way to move forward with markers and locations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,26 @@
 let map;
-var service;
-var infowindow;
 
+let townsVillages = [{
+    coords: {
+        /* Orford */
+        lat: 52.0951785,
+        lng: 1.5257991,
+    },
+    content: `<h4>Orford</h4>
+    <h6>Woodbridge</h6>
+    <div class ="info-content">
+    <p>Orford is a village with historic town status in Suffolk, England, within the Suffolk Coast and Heaths AONB. It is located 9 miles east of Woodbridge.</p>
+    </div>`
+},
+    coords: {
+        /* aldeburgh */
+        lat: 52.156743,
+        lng: 1.5724309,
+    }
+}]
 function initMap() {
-    var suffolkCoast = new google.maps.LatLng(52.1823, 1.6130);
   map = new google.maps.Map(document.getElementById("map"), {
-    center: {  lat: 52.1823, lng: 1.6130 },
+    center: { lat: 52.1823, lng: 1.6130 },
     zoom: 10,
-    locationBias: {radius: 50000, lat: 52.1403, lng: 1.4114  }
   });
-
-  var request = {
-    location: suffolkCoast,
-    radius: '5000',
-    type: ['restaurant']
-  };
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-  function createMarkers(places) {
-      places.forEach(place => {
-        let marker = new google.maps.Marker({
-            position: place.location,
-            map: map,
-            title: place.name,
-        });
-         suffolkCoast.extend(place.geometry.location);
-      });
-      map.fitBounds(suffolkCoast);
-    }
-     
 }
