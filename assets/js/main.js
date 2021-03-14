d61dcc24ed69a053eb59c41f1ff439781914c020
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-03-14 22:21:38 +0000

    Removing and retrying new way of getting markers in place

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,47 +3,37 @@ var service;
 var infowindow;
 
 function initMap() {
-    const suffolkCoastal = new google.maps.LatLng (52.1539301, 1.4746412);
+    var suffolkCoast = new google.maps.LatLng(52.1823, 1.6130);
   map = new google.maps.Map(document.getElementById("map"), {
-    center: suffolkCoastal,
+    center: {  lat: 52.1823, lng: 1.6130 },
     zoom: 10,
+    locationBias: {radius: 50000, lat: 52.1403, lng: 1.4114  }
   });
 
   var request = {
-      location: suffolkCoastal,
-      radius: '1000',
-      type: ['restaurant']
+    location: suffolkCoast,
+    radius: '5000',
+    type: ['restaurant']
   };
-
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
 }
-
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
     }
-}
-}
-
-   /* const contentString = '<h1>The Suffolk Coast</h1>';
-    const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });
-
-    var marker = new google.maps.Marker({
-        position: suffolkCoastal,
-        map: map,
-        title: "Suffolk Coast",
+  }
+  function createMarkers(places) {
+      places.forEach(place => {
+        let marker = new google.maps.Marker({
+            position: place.location,
+            map: map,
+            title: place.name,
         });
+         suffolkCoast.extend(place.geometry.location);
+      });
+      map.fitBounds(suffolkCoast);
+    }
      
-        marker.addListener("click", () => {
-    infowindow.open(map, marker);
-  });
-      } */ 
-
-
-
-
-
+}
