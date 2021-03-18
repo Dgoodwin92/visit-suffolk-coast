c74e1e10a7c9412cd9d58de4e687caf82e2ca707
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-03-18 21:26:11 +0000

    Created email response, contact.js has been created. videos added to index.html with footers added to all pages. Responsiveness checked and working.

---
+++ assets/js/contact.js
@@ -0,0 +1,19 @@
+function sendMail(){
+    let fullName = document.getElementById("userName").value;
+    let userEmail = document.getElementById("userEmail").value;
+    let userMessage = document.getElementById("message").value;
+
+    var contactParams = {
+        from_name: fullName,
+        from_email: userEmail,
+        message: userMessage
+    };
+
+    emailjs.send('Suffolk-Coast','SuffolkCoastMail', contactParams)
+    .then(function(response) {
+       alert('Email has succesfully sent', response.status, response.text);
+    }, function(error) {
+       alert('EMAIL FAILED TO SEND...', error);
+    });
+}
+
